d77f7c837ed84a749d9796f19039c99cf88d2ebe
Anson Foong <afoong@albany.edu>
2019-10-03 04:39:39 -0400

    Pass in command_prefix to constructor.

--- src/SubscribeBot.py
+++ src/SubscribeBot.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
-class SubscriberBot(commands.Bot):
+class SubscribeBot(commands.Bot):
+
+    def __init__(self, command_prefix):
+        super().__init__(command_prefix=command_prefix)
+
     async def on_ready(self):
         print("Ready")
+    
