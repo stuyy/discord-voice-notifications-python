7955d32333c8b946b02330c822201083d8a399cb
Anson Foong <afoong@albany.edu>
2019-10-03 06:53:34 -0400

    Parse args and check if user passed channel id or name.

--- src/cogs/Subscribe.py
+++ src/cogs/Subscribe.py
@@ -1,13 +1,40 @@
 from discord.ext import commands
-
+import discord
 class Subscribe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases=['sub', 's'], invoke_without_command=True)
-    async def subscribe(self, ctx):
-        print('subscribing')
+    @commands.group(aliases=['sub', 's'], invoke_without_command=True, case_insensitive=True)
+    async def subscribe(self, ctx, *args):
 
+        guild = discord.utils.find(lambda g : g.id == int(ctx.guild.id), self.bot.guilds)
+
+        if guild is not None:
+            valid_voice_channels = {} # List of all IDs to subscribe user to.
+            for arg in args:
+                # Find by ID first, if not found, find by name.
+                try:
+                    ch = discord.utils.find(lambda c : c.id == int(arg), guild.voice_channels)
+                    if ch is not None:
+                        if str(ch.id) in valid_voice_channels:
+                            pass
+                        else:
+                            valid_voice_channels[ch.id] = ch
+
+                except Exception as error:
+                    print(error)
+                    ch = discord.utils.find(lambda c : c.name == arg, guild.voice_channels)
+                    if ch is not None:
+                        if str(ch.id) in valid_voice_channels:
+                            pass
+                        else:
+                            valid_voice_channels[str(ch.id)] = ch
+
+            
+        else:
+            print('Invalid.')
+
+    
     @commands.group(aliases=['unsub'], invoke_without_command=True)
     async def unsubscribe(self, ctx):
         print('unsub')
@@ -23,7 +50,6 @@ class Subscribe(commands.Cog):
     @unsubscribe.command(name='all')
     async def unsub_all(self, ctx):
         print('unsub all')
-
 
 def setup(bot):
     bot.add_cog(Subscribe(bot))
