7ffe36fd59154867f33ce4699899abfaee35cd4a
Anson Foong <afoong@albany.edu>
2019-10-03 07:44:58 -0400

    Unsubscribe function will remove keys from the channels dict in the GuildMember document.

--- src/database.py
+++ src/database.py
@@ -17,3 +17,21 @@ class Database:
             member_doc = query[0]
             member_doc.channels.update(channels)
             member_doc.update(set__channels=member_doc.channels)
+
+    def unsubscribe(self, channels, ctx):
+        member = ctx.author
+        # Get the member document first.
+        query = GuildMember.objects(user_id=str(member.id))
+        if len(query) == 0:
+            pass
+        else:
+            member_doc = query[0]
+            subscribed = member_doc.channels
+            print(subscribed)
+            channel_ids = channels.keys()
+            for ids in channel_ids:
+                if ids in channel_ids:
+                    print('yes')
+                    member_doc.channels.pop(ids, None)
+            
+            member_doc.update(set__channels=member_doc.channels)
