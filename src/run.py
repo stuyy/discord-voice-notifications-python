239c3d54ef1e06b43b90fceae4e401ccdb84dda6
Anson Foong <afoong@albany.edu>
2019-10-03 08:58:23 -0400

    Updating pipfile..

--- src/run.py
+++ src/run.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 from discord.ext import commands
 from SubscribeBot import SubscribeBot
 from database import Database
 import os
+
+load_dotenv()
 
 db = Database()
 
@@ -11,5 +14,5 @@ exts = ['cogs.Subscribe', 'cogs.Whitelist']
 if __name__ == '__main__':
     for ext in exts:
         client.load_extension(ext)
-
+print(os.getenv("YEET"))
 client.run(os.getenv("TOKEN"))
