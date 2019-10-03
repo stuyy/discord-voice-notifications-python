136b7ac0284cb15efed32e16e79ef9bb7fa8cf3f
Anson Foong <afoong@albany.edu>
2019-10-03 07:30:05 -0400

    Add user document to db and set their subscribed channels.

--- src/database.py
+++ src/database.py
@@ -1,8 +1,19 @@
 from mongoengine import connect
+from models.MemberCollections import GuildMember
 
-class SubscribeDatabase:
+class Database:
     def __init__(self, name='subscriberbot'):
         self.connect_db(name)
 
     def connect_db(self, name):
         connect(name)
+    def subscribe(self, channels, ctx):
+        member = ctx.author
+        query = GuildMember.objects(user_id=str(member.id))
+        if len(query) == 0:
+            member_doc = GuildMember(str(member.id), str(member.name), {}, channels)
+            member_doc.save()
+        else:
+            member_doc = query[0]
+            member_doc.channels.update(channels)
+            member_doc.update(set__channels=member_doc.channels)
