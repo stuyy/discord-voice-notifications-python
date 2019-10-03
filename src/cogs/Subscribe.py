e9299a654770a73ccdd88f175e6a41635cb7d266
Anson Foong <afoong@albany.edu>
2019-10-03 05:08:10 -0400

    Added subscribe commands

--- src/cogs/Subscribe.py
+++ src/cogs/Subscribe.py
@@ -4,10 +4,26 @@ class Subscribe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group(aliases=['sub', 's'], invoke_without_command=True)
     async def subscribe(self, ctx):
-        print('hi')
+        print('subscribing')
+
+    @commands.group(aliases=['unsub'], invoke_without_command=True)
+    async def unsubscribe(self, ctx):
+        print('unsub')
 
     @commands.command()
+    async def subbed(self, ctx):
+        print('display subbed')
+
+    @subscribe.command(name='all')
+    async def sub_all(self, ctx):
+        print('sub all')
+
+    @unsubscribe.command(name='all')
+    async def unsub_all(self, ctx):
+        print('unsub all')
+
+
 def setup(bot):
     bot.add_cog(Subscribe(bot))
