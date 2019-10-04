ef3ca70135cc8e6be582b8776a3251c5691f2b62
Anson Foong <afoong@albany.edu>
2019-10-04 07:26:14 -0400

    added minor changes

--- src/run.py
+++ src/run.py
@@ -4,15 +4,19 @@ from SubscribeBot import SubscribeBot
 from database import Database
 import os
 
-load_dotenv()
 
 db = Database()
 
 client = SubscribeBot(db, command_prefix='?', case_insensitive=True)
-exts = ['cogs.Subscribe', 'cogs.Whitelist']
+exts = ['cogs.Subscribe', 'cogs.Whitelist', 'cogs.RoleReactions']
 
 if __name__ == '__main__':
     for ext in exts:
         client.load_extension(ext)
-print(os.getenv("YEET"))
+    
+    if os.getenv("MODE") == "DEV":
+        print("DEV MODE")
+    elif os.getenv("MODE") == "PROD":
+        load_dotenv()
+
 client.run(os.getenv("TOKEN"))
