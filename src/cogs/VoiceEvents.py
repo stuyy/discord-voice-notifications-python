fefa32b630bdc9152c6f5bd9e60330e3bcc457f1
Anson Foong <afoong@albany.edu>
2019-10-14 21:46:03 -0400

    basic checks for the state of a user and their voice state.

--- src/cogs/VoiceEvents.py
+++ src/cogs/VoiceEvents.py
@@ -6,8 +6,13 @@ class VoiceEvents(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        print(member.name)
-
-    
+        
+        if before.channel is None and after.channel is not None:
+            print(member.name + " just joined a channel!")
+        elif before.channel is not None and after.channel is not None:
+            print("{} switched from {} to {}".format(member.name, before.channel.name, after.channel.name))
+        elif before.channel is not None and after.channel is None:
+            print("{} left {}".format(member.name, before.channel.name))
+        
 def setup(bot):
     bot.add_cog(VoiceEvents(bot))
