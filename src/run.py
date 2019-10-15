d8adde3394152777a7ebc906290225c7162e304a
Anson Foong <afoong@albany.edu>
2019-10-15 13:16:35 -0400

    Load extensions by reading cogs directory.

--- src/run.py
+++ src/run.py
@@ -4,15 +4,18 @@ from SubscribeBot import SubscribeBot
 from database import Database
 import os
 
-
 db = Database()
 
 client = SubscribeBot(db, command_prefix='?', case_insensitive=True)
 exts = ['cogs.Subscribe', 'cogs.Whitelist', 'cogs.RoleReactions', 'cogs.VoiceEvents']
 
 if __name__ == '__main__':
-    for ext in exts:
-        client.load_extension(ext)
+    for f in os.listdir("./src/cogs"):
+        p = f.find(".py")
+        if p != -1:
+            ext = "cogs." + f[0:p]
+            client.load_extension(ext)
+            print("Loaded " + ext)
     
     if os.getenv("MODE") == "DEV":
         print("DEV MODE")
